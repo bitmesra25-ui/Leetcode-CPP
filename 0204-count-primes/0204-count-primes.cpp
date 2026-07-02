960231288af0ebf1b7fcb5121967bae1743dfdc1
ROHIT RANJAN <bitmesra25@gmail.com>
2026-07-02 20:22:07 +0530

    Time: 199 ms (42.19%), Space: 12.6 MB (91.29%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<2)
+        return 0;
+        vector<bool>prime(n,true);
+        int count=0;
+        for(int i=2;i<n;++i){
+            if(prime[i] ){
+                count++;
+                if((long long)i*i<n ){
+                for(int j=i*i;j<n;j+=i)prime[j]=false;
+                }
+            }
+        }
+        return count;
+    }
+};
