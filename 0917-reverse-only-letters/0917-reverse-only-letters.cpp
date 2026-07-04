4d6eddedc3cdb844da5e642446dcded85a44ef95
ROHIT RANJAN <bitmesra25@gmail.com>
2026-07-04 21:15:23 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.48%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            while(left<right && !isalpha(s[left])){
+                left++;
+            }
+            while(left<right && !isalpha(s[right])){
+                right--;
+            }
+            if(left<right){
+                swap(s[left],s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
