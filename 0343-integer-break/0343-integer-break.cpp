a6d63177af13421a2f93942485343a691e9aee0a
ROHIT RANJAN <bitmesra25@gmail.com>
2026-08-19 20:39:33 +0530

    Time: 0 ms (100%), Space: 8.2 MB (54.82%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        int num=n/3;
+        int rem=n%3;
+        if(rem==0){
+             return pow(3,num);
+        }
+        if(rem==1){
+             return pow(3,num-1)*4;
+        }
+        else
+         return pow(3,num)*2;
+    }
+};
